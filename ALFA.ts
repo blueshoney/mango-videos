e094ee3e16f3bd4b409bc7a9be6c25278e16747b
blueshoney <71082739+blueshoney@users.noreply.github.com>
2025-11-04 16:08:55 +0300

    Add files via upload

---
+++ ALFA.ts
@@ -0,0 +1,161 @@
+#EXTM3U
+#EXTINF:-1 tvg-ID="alfaiptv" user-agent="okhttp/4.10.0" tvg-name="Alfa IPTV Server" tvg-logo="https://easilycdn.net/images/live/5f75df63.png" group-title="TR: Spor",Alfa IPTV Server
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/1
+#EXTINF:-1 tvg-ID="efef0ea19b7b6a76335a7fdd979788de" user-agent="okhttp/4.10.0" tvg-name="SD: beIN Sports 1" tvg-logo="https://easilycdn.net/images/live/sd-1.png" group-title="TR: Spor",SD: beIN Sports 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/2
+#EXTINF:-1 tvg-ID="9472cf7603f5177a6d4a25480e8735e3" user-agent="okhttp/4.10.0" tvg-name="SD: beIN Sports 2" tvg-logo="https://easilycdn.net/images/live/sd-2.png" group-title="TR: Spor",SD: beIN Sports 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/3
+#EXTINF:-1 tvg-ID="79c38c00f6cd5ffdfe5d28cac600e987" user-agent="okhttp/4.10.0" tvg-name="SD: beIN Sports 3" tvg-logo="https://easilycdn.net/images/live/sd-3.png" group-title="TR: Spor",SD: beIN Sports 3
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/4
+#EXTINF:-1 tvg-ID="ffb8dba4c4d25f5cc46415b3cabd2eb3" user-agent="okhttp/4.10.0" tvg-name="SD: beIN Sports 4" tvg-logo="https://easilycdn.net/images/live/sd-4.png" group-title="TR: Spor",SD: beIN Sports 4
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/5
+#EXTINF:-1 tvg-ID="c4d538e1b764e32af7865a28419e554f" user-agent="okhttp/4.10.0" tvg-name="SD: beIN Sports 5" tvg-logo="https://easilycdn.net/images/live/sd-5.png" group-title="TR: Spor",SD: beIN Sports 5
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/68627
+#EXTINF:-1 tvg-ID="1c828f444bec6ace46d8aaa3966f1775" user-agent="okhttp/4.10.0" tvg-name="SD: beIN Sports MAX 1" tvg-logo="https://easilycdn.net/images/live/sd-max-1.png" group-title="TR: Spor",SD: beIN Sports MAX 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/6
+#EXTINF:-1 tvg-ID="6221aa6f1455d44f90b6976f7ae5d612" user-agent="okhttp/4.10.0" tvg-name="SD: beIN Sports MAX 2" tvg-logo="https://easilycdn.net/images/live/sd-max-2.png" group-title="TR: Spor",SD: beIN Sports MAX 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/7
+#EXTINF:-1 tvg-ID="efef0ea19b7b6a76335a7fdd979788de" user-agent="okhttp/4.10.0" tvg-name="HD: beIN Sports 1" tvg-logo="https://easilycdn.net/images/live/hd-1.png" group-title="TR: Spor",HD: beIN Sports 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/9
+#EXTINF:-1 tvg-ID="9472cf7603f5177a6d4a25480e8735e3" user-agent="okhttp/4.10.0" tvg-name="HD: beIN Sports 2" tvg-logo="https://easilycdn.net/images/live/hd-2.png" group-title="TR: Spor",HD: beIN Sports 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/10
+#EXTINF:-1 tvg-ID="79c38c00f6cd5ffdfe5d28cac600e987" user-agent="okhttp/4.10.0" tvg-name="HD: beIN Sports 3" tvg-logo="https://easilycdn.net/images/live/hd-3.png" group-title="TR: Spor",HD: beIN Sports 3
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/11
+#EXTINF:-1 tvg-ID="ffb8dba4c4d25f5cc46415b3cabd2eb3" user-agent="okhttp/4.10.0" tvg-name="HD: beIN Sports 4" tvg-logo="https://easilycdn.net/images/live/hd-4.png" group-title="TR: Spor",HD: beIN Sports 4
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/12
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="HD: beIN Sports 5" tvg-logo="https://easilycdn.net/images/live/hd-5.png" group-title="TR: Spor",HD: beIN Sports 5
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/68628
+#EXTINF:-1 tvg-ID="1c828f444bec6ace46d8aaa3966f1775" user-agent="okhttp/4.10.0" tvg-name="HD: beIN Sports MAX 1" tvg-logo="https://easilycdn.net/images/live/hd-max-1.png" group-title="TR: Spor",HD: beIN Sports MAX 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/13
+#EXTINF:-1 tvg-ID="6221aa6f1455d44f90b6976f7ae5d612" user-agent="okhttp/4.10.0" tvg-name="HD: beIN Sports MAX 2" tvg-logo="https://easilycdn.net/images/live/hd-max-2.png" group-title="TR: Spor",HD: beIN Sports MAX 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/14
+#EXTINF:-1 tvg-ID="efef0ea19b7b6a76335a7fdd979788de" user-agent="okhttp/4.10.0" tvg-name="Full HD: beIN Sports 1" tvg-logo="https://easilycdn.net/images/live/fullhd-1.png" group-title="TR: Spor",Full HD: beIN Sports 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/16
+#EXTINF:-1 tvg-ID="9472cf7603f5177a6d4a25480e8735e3" user-agent="okhttp/4.10.0" tvg-name="Full HD: beIN Sports 2" tvg-logo="https://easilycdn.net/images/live/fullhd-2.png" group-title="TR: Spor",Full HD: beIN Sports 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/17
+#EXTINF:-1 tvg-ID="79c38c00f6cd5ffdfe5d28cac600e987" user-agent="okhttp/4.10.0" tvg-name="Full HD: beIN Sports 3" tvg-logo="https://easilycdn.net/images/live/fullhd-3.png" group-title="TR: Spor",Full HD: beIN Sports 3
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/18
+#EXTINF:-1 tvg-ID="ffb8dba4c4d25f5cc46415b3cabd2eb3" user-agent="okhttp/4.10.0" tvg-name="Full HD: beIN Sports 4" tvg-logo="https://easilycdn.net/images/live/fullhd-4.png" group-title="TR: Spor",Full HD: beIN Sports 4
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/19
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="Full HD: beIN Sports 5" tvg-logo="https://easilycdn.net/images/live/fullhd-5.png" group-title="TR: Spor",Full HD: beIN Sports 5
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/68629
+#EXTINF:-1 tvg-ID="1c828f444bec6ace46d8aaa3966f1775" user-agent="okhttp/4.10.0" tvg-name="Full HD: beIN Sports MAX 1" tvg-logo="https://easilycdn.net/images/live/fullhd-max-1.png" group-title="TR: Spor",Full HD: beIN Sports MAX 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/20
+#EXTINF:-1 tvg-ID="6221aa6f1455d44f90b6976f7ae5d612" user-agent="okhttp/4.10.0" tvg-name="Full HD: beIN Sports MAX 2" tvg-logo="https://easilycdn.net/images/live/fullhd-max-2.png" group-title="TR: Spor",Full HD: beIN Sports MAX 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/21
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="4K: beIN Sports HEVC" tvg-logo="https://easilycdn.net/images/live/bs-4k.png" group-title="TR: Spor",4K: beIN Sports HEVC
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/91015
+#EXTINF:-1 tvg-ID="54efd880b0d7a3a8c0026651b1c60f2c" user-agent="okhttp/4.10.0" tvg-name="TR: S Sport 1" tvg-logo="https://easilycdn.net/images/live/5b59b6a0.png" group-title="TR: Spor",TR: S Sport 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/47
+#EXTINF:-1 tvg-ID="3e80a3f3f251e72da2e6247eea1fb6c8" user-agent="okhttp/4.10.0" tvg-name="TR: S Sport 2" tvg-logo="https://easilycdn.net/images/live/add44a8c.png" group-title="TR: Spor",TR: S Sport 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/50
+#EXTINF:-1 tvg-ID="4fa5cb5cf46dd23c83a45308cf3d5870" user-agent="okhttp/4.10.0" tvg-name="TR: TV8.5" tvg-logo="https://easilycdn.net/images/live/8b67aadb.png" group-title="TR: Spor",TR: TV8.5
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/73
+#EXTINF:-1 tvg-ID="fad9d8382dde21db2ad7c74b9ea957ed" user-agent="okhttp/4.10.0" tvg-name="TR: NBA TV" tvg-logo="https://easilycdn.net/images/live/c633af7e.png" group-title="TR: Spor",TR: NBA TV
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/55
+#EXTINF:-1 tvg-ID="4642af4cac79d783dfa6e0ba45c0405a" user-agent="okhttp/4.10.0" tvg-name="TR: Spor Smart 1" tvg-logo="https://easilycdn.net/images/live/1e05831a.png" group-title="TR: Spor",TR: Spor Smart 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/43
+#EXTINF:-1 tvg-ID="52f9866155cc12dd93220977cc0aa5fc" user-agent="okhttp/4.10.0" tvg-name="TR: Spor Smart 2" tvg-logo="https://easilycdn.net/images/live/a67a3c8b.png" group-title="TR: Spor",TR: Spor Smart 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/45
+#EXTINF:-1 tvg-ID="31728da75922148a771a4e1b3535f8bd" user-agent="okhttp/4.10.0" tvg-name="TR: Tivibu Spor 1" tvg-logo="https://easilycdn.net/images/live/bc4c1a33.png" group-title="TR: Spor",TR: Tivibu Spor 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/36
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Tivibu Spor 2" tvg-logo="https://easilycdn.net/images/live/d2fe6d53.png" group-title="TR: Spor",TR: Tivibu Spor 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/37
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Tivibu Spor 3" tvg-logo="https://easilycdn.net/images/live/6564008f.png" group-title="TR: Spor",TR: Tivibu Spor 3
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/38
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Tivibu Spor 4" tvg-logo="https://easilycdn.net/images/live/0e9a8293.png" group-title="TR: Spor",TR: Tivibu Spor 4
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/39
+#EXTINF:-1 tvg-ID="a4f3e253b3459b2d960d86fb96c46bbb" user-agent="okhttp/4.10.0" tvg-name="TR: Eurosport 1" tvg-logo="https://easilycdn.net/images/live/6bfa898f.png" group-title="TR: Spor",TR: Eurosport 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/40
+#EXTINF:-1 tvg-ID="bc91e09f0cc76a06f0145d83879a7cc6" user-agent="okhttp/4.10.0" tvg-name="TR: Eurosport 2" tvg-logo="https://easilycdn.net/images/live/0dabeec7.png" group-title="TR: Spor",TR: Eurosport 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/41
+#EXTINF:-1 tvg-ID="2d8b2af3693214d805edc509ef9a0caa" user-agent="okhttp/4.10.0" tvg-name="TR: Cosmo Sports" tvg-logo="https://easilycdn.net/images/live/2b01c65b.png" group-title="TR: Spor",TR: Cosmo Sports
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/82707
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Fight Network" tvg-logo="https://easilycdn.net/images/live/e8ba1d22.png" group-title="TR: Spor",TR: Fight Network
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/59
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: W-Sport" tvg-logo="https://easilycdn.net/images/live/a7acb11d.png" group-title="TR: Spor",TR: W-Sport
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/82709
+#EXTINF:-1 tvg-ID="8fb123f4e1d846f8c05d3118bfb16d6e" user-agent="okhttp/4.10.0" tvg-name="TR: A Spor" tvg-logo="https://easilycdn.net/images/live/72893c62.png" group-title="TR: Spor",TR: A Spor
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/56
+#EXTINF:-1 tvg-ID="29b8292890bf32e3e37f0cff141786f9" user-agent="okhttp/4.10.0" tvg-name="TR: HTSpor" tvg-logo="https://easilycdn.net/images/live/2bde1ff3.png" group-title="TR: Spor",TR: HTSpor
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/86042
+#EXTINF:-1 tvg-ID="cffde168be2d13548010297c4206e4ae" user-agent="okhttp/4.10.0" tvg-name="TR: TRT Spor" tvg-logo="https://easilycdn.net/images/live/0795fa84.png" group-title="TR: Spor",TR: TRT Spor
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/57
+#EXTINF:-1 tvg-ID="007fb54ecc87427206d661031f36f195" user-agent="okhttp/4.10.0" tvg-name="TR: TRT 3 Spor" tvg-logo="https://easilycdn.net/images/live/86dd242.png" group-title="TR: Spor",TR: TRT 3 Spor
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/28745
+#EXTINF:-1 tvg-ID="2f2459a13975d94cd94720257a052ae1" user-agent="okhttp/4.10.0" tvg-name="TR: TRT Spor Yıldız" tvg-logo="https://easilycdn.net/images/live/55228ad8.png" group-title="TR: Spor",TR: TRT Spor Yıldız
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/58
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: TAY TV" tvg-logo="https://easilycdn.net/images/live/ba5e8bf4.png" group-title="TR: Spor",TR: TAY TV
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/62
+#EXTINF:-1 tvg-ID="ef9093ec190c798042adb17f74860f30" user-agent="okhttp/4.10.0" tvg-name="TR: TJK TV" tvg-logo="https://easilycdn.net/images/live/17751a45.png" group-title="TR: Spor",TR: TJK TV
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/82710
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: FB TV" tvg-logo="https://easilycdn.net/images/live/84a1da52.png" group-title="TR: Spor",TR: FB TV
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/63
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Sports TV" tvg-logo="https://easilycdn.net/images/live/610122a7.png" group-title="TR: Spor",TR: Sports TV
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/65
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Redbull Tv" tvg-logo="https://easilycdn.net/images/live/34f4cbe6.png" group-title="TR: Spor",TR: Redbull Tv
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/93677
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Ekol Sports" tvg-logo="https://easilycdn.net/images/live/5c719aa2.png" group-title="TR: Spor",TR: Ekol Sports
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/93676
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: beIN Sports Haber" tvg-logo="https://easilycdn.net/images/live/f6e8e1d1.png" group-title="TR: Spor",TR: beIN Sports Haber
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/8
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: S Sport Plus 1" tvg-logo="https://easilycdn.net/images/live/41362d6f.png" group-title="TR: Spor",TR: S Sport Plus 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/52
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: S Sport Plus 2" tvg-logo="https://easilycdn.net/images/live/41362d6f.png" group-title="TR: Spor",TR: S Sport Plus 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/53
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: S Sport Plus 3" tvg-logo="https://easilycdn.net/images/live/41362d6f.png" group-title="TR: Spor",TR: S Sport Plus 3
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/54
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: S Sport Plus 4" tvg-logo="https://easilycdn.net/images/live/41362d6f.png" group-title="TR: Spor",TR: S Sport Plus 4
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/28416
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: S Sport Plus 5" tvg-logo="https://easilycdn.net/images/live/41362d6f.png" group-title="TR: Spor",TR: S Sport Plus 5
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/28417
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: S Sport Plus 6" tvg-logo="https://easilycdn.net/images/live/41362d6f.png" group-title="TR: Spor",TR: S Sport Plus 6
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/28418
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Tabii Spor" tvg-logo="https://easilycdn.net/images/live/tabii.png" group-title="TR: Spor",TR: Tabii Spor
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/86019
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Tabii Spor 1" tvg-logo="https://easilycdn.net/images/live/tabii.png" group-title="TR: Spor",TR: Tabii Spor 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/85448
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Tabii Spor 2" tvg-logo="https://easilycdn.net/images/live/tabii.png" group-title="TR: Spor",TR: Tabii Spor 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/85449
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Tabii Spor 3" tvg-logo="https://easilycdn.net/images/live/tabii.png" group-title="TR: Spor",TR: Tabii Spor 3
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/85450
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Tabii Spor 4" tvg-logo="https://easilycdn.net/images/live/tabii.png" group-title="TR: Spor",TR: Tabii Spor 4
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/86016
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Tabii Spor 5" tvg-logo="https://easilycdn.net/images/live/tabii.png" group-title="TR: Spor",TR: Tabii Spor 5
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/86017
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Tabii  Spor 6" tvg-logo="https://easilycdn.net/images/live/tabii.png" group-title="TR: Spor",TR: Tabii  Spor 6
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/86018
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: beIN Connect 1" tvg-logo="https://easilycdn.net/images/live/bs-connect.png" group-title="TR: Spor",TR: beIN Connect 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/93720
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: beIN Connect 2" tvg-logo="https://easilycdn.net/images/live/bs-connect.png" group-title="TR: Spor",TR: beIN Connect 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/93721
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: beIN Connect 3" tvg-logo="https://easilycdn.net/images/live/bs-connect.png" group-title="TR: Spor",TR: beIN Connect 3
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/93722
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: beIN Connect 4" tvg-logo="https://easilycdn.net/images/live/bs-connect.png" group-title="TR: Spor",TR: beIN Connect 4
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/93723
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: beIN Connect 5" tvg-logo="https://easilycdn.net/images/live/bs-connect.png" group-title="TR: Spor",TR: beIN Connect 5
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/93724
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: beIN Connect 6" tvg-logo="https://easilycdn.net/images/live/bs-connect.png" group-title="TR: Spor",TR: beIN Connect 6
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/93725
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: beIN Connect 7" tvg-logo="https://easilycdn.net/images/live/bs-connect.png" group-title="TR: Spor",TR: beIN Connect 7
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/93726
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: beIN Connect 8" tvg-logo="https://easilycdn.net/images/live/bs-connect.png" group-title="TR: Spor",TR: beIN Connect 8
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/93727
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: beIN Connect 9" tvg-logo="https://easilycdn.net/images/live/bs-connect.png" group-title="TR: Spor",TR: beIN Connect 9
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/93728
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Exxen Sports 1" tvg-logo="https://easilycdn.net/images/live/d6308fe6.png" group-title="TR: Spor",TR: Exxen Sports 1
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/705
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Exxen Sports 2" tvg-logo="https://easilycdn.net/images/live/d6308fe6.png" group-title="TR: Spor",TR: Exxen Sports 2
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/1530
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Exxen Sports 3" tvg-logo="https://easilycdn.net/images/live/d6308fe6.png" group-title="TR: Spor",TR: Exxen Sports 3
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/1531
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Exxen Sports 4" tvg-logo="https://easilycdn.net/images/live/d6308fe6.png" group-title="TR: Spor",TR: Exxen Sports 4
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/5655
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Exxen Sports 5" tvg-logo="https://easilycdn.net/images/live/d6308fe6.png" group-title="TR: Spor",TR: Exxen Sports 5
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/5656
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Exxen Sports 6" tvg-logo="https://easilycdn.net/images/live/d6308fe6.png" group-title="TR: Spor",TR: Exxen Sports 6
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/5657
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Exxen Sports 7" tvg-logo="https://easilycdn.net/images/live/d6308fe6.png" group-title="TR: Spor",TR: Exxen Sports 7
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/67838
+#EXTINF:-1 tvg-ID="" user-agent="okhttp/4.10.0" tvg-name="TR: Exxen Sports 8" tvg-logo="https://easilycdn.net/images/live/d6308fe6.png" group-title="TR: Spor",TR: Exxen Sports 8
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/67839
