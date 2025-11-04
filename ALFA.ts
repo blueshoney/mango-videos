7ffa3dc071f3307a3492d3e3799ded50db80a43a
blueshoney <71082739+blueshoney@users.noreply.github.com>
2025-11-04 16:09:44 +0300

    Add files via upload

---
+++ ALFA.ts
@@ -0,0 +1,3 @@
+#EXTM3U
+#EXTINF:-1 tvg-ID="79c38c00f6cd5ffdfe5d28cac600e987" user-agent="okhttp/4.10.0" tvg-name="HD: beIN Sports 3" tvg-logo="https://easilycdn.net/images/live/hd-3.png" group-title="TR: Spor",HD: beIN Sports 3
+http://s63v37.one:80/c78t53g86r72/1952x23022025x1990x/11
